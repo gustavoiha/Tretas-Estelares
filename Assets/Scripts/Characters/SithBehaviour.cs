f4ca9031044b0278edfa6f0a19c5dd8d8153e766
Gustavo Iha <gustavoiha@gmail.com>
2016-03-27 22:03:18 -0300

    Improvements on camera's behaviour

--- Assets/Scripts/Characters/SithBehaviour.cs
+++ Assets/Scripts/Characters/SithBehaviour.cs
@@ -23,6 +23,10 @@ public class SithBehaviour : MonoBehaviour {
 	public float CameraAngle = 35.0f;
 	public float CameraLookAngle = 10.0f;
 
+	// Camera rotation limits. Upper limit should be positive, and Lower limit should be negative!
+	public float CameraAngleLimitUpper = 85.0f;
+	public float CameraAngleLimitLower = -10.0f;
+
 	// Camera's distance from Pivot
 	public float CameraDistance = 9.5f;
 
@@ -30,6 +34,9 @@ public class SithBehaviour : MonoBehaviour {
 	public float moveSpeedSides  = 6.0f;
 	public float turnSpeedX = 60.0f;
 	public float turnSpeedY = 60.0f;
+
+	private int mouseInvertX = 1;
+	private int mouseInvertY = -1;
 
 	private Vector3 moveDirection = Vector3.zero;
 	public float gravity = 20.0f;
@@ -62,6 +69,7 @@ public class SithBehaviour : MonoBehaviour {
 
 		doTranslation ();
 		doRotation ();
+		updateCameraCoordinates ();
 	}
 
 	private void doTranslation(){
@@ -85,18 +93,35 @@ public class SithBehaviour : MonoBehaviour {
 		Quaternion quaternion  = new Quaternion ();
 		quaternion.eulerAngles = new Vector3 (0, transform.rotation.eulerAngles.y, 0);
 
-		float turnX = Input.GetAxis ("Mouse X") + Input.GetAxis ("HorizontalRotation");
-		float turnY = Input.GetAxis ("Mouse Y") + Input.GetAxis ("VerticalRotation");
+		float turnX = Input.GetAxis ("Mouse X") * Mathf.Sign(mouseInvertX) + Input.GetAxis ("HorizontalRotation");
 
 		transform.rotation = quaternion;
 		transform.Rotate (0, turnX * turnSpeedX * Time.deltaTime, 0);
+	}
 
-		//float camPosX = - CameraDistance * Mathf.Cos(camera.rotation.eulerAngles.y * Mathf.Deg2Rad) * Mathf.Cos(camera.rotation.eulerAngles.x * Mathf.Deg2Rad);
+	private void updateCameraCoordinates(){
+		
+		float turnY = Input.GetAxis ("Mouse Y") * Mathf.Sign(mouseInvertY) + Input.GetAxis ("VerticalRotation");
+
 		float camPosY = CameraPivot + CameraDistance * Mathf.Sin(camera.rotation.eulerAngles.x * Mathf.Deg2Rad) * Mathf.Cos(camera.rotation.eulerAngles.z * Mathf.Deg2Rad);
-		float camPosZ = - CameraDistance * Mathf.Cos(camera.rotation.eulerAngles.x * Mathf.Deg2Rad) * Mathf.Cos(camera.rotation.eulerAngles.z * Mathf.Deg2Rad);;
+		float camPosZ = -CameraDistance * Mathf.Cos(camera.rotation.eulerAngles.x * Mathf.Deg2Rad) * Mathf.Cos(camera.rotation.eulerAngles.z * Mathf.Deg2Rad);;
 
 		camera.Rotate (turnY * turnSpeedY * Time.deltaTime, 0, 0);
 		camera.localPosition = new Vector3 (0, camPosY, camPosZ);
+
+		Debug.Log (camera.localRotation.eulerAngles);
+
+		// Angle to limit the camera's rotation
+		float newCamAngleX;
+
+		if (camera.rotation.eulerAngles.x <= 180)
+			newCamAngleX = Mathf.Min (camera.localRotation.eulerAngles.x, CameraAngleLimitUpper);
+		else
+			newCamAngleX = Mathf.Max (camera.localRotation.eulerAngles.x, 360+CameraAngleLimitLower);
+
+		Quaternion quaternion = new Quaternion ();
+		quaternion.eulerAngles = new Vector3 (newCamAngleX, 0, 0);
+		camera.localRotation = quaternion;
 	}
 
 	// returns true if player is in walking or running animation
@@ -105,5 +130,16 @@ public class SithBehaviour : MonoBehaviour {
 			animator.GetCurrentAnimatorStateInfo (0).IsName ("running") ||
 			animator.GetCurrentAnimatorStateInfo (0).IsName ("walkingSaber") ||
 			animator.GetCurrentAnimatorStateInfo (0).IsName ("runningSaber");
+	}
+
+	/**
+	 * Invert mouse directions regarding the camera's rotation
+	 * pass "X" as argument to invert horizontal axis, and "Y" for vertical
+	 */
+	public void invertMouseDirection(string direction){
+		if (direction == "X")
+			mouseInvertX *= -1;
+		else if (direction == "Y")
+			mouseInvertY *= -1;
 	}
 }
